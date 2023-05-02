e1fd37642295703be6d1083ea30e7043d3dc002f
Nicolas Fuentes <nfuentes11@gmail.com>
2023-05-01 22:47:58 -0400

    Js formula

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const reverseSeq = (n) => {
-  let result = [];
-  for (let i = n; i <= 1; i--) {
-    result.push(i);
-  }
-  return result;
-};
-console.log(reverseSeq(5));
+function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const billableDaysPerMonth = 22;
+  const months = Math.floor(numDays / billableDaysPerMonth);
+  const percentageAfter = 1 - discount;
+  const remainingDays = numDays % billableDaysPerMonth;
+  const monthCost = billableDaysPerMonth * ratePerHour * percentageAfter;
+  const fullMonthsCost = months * monthCost;
+  const remainingCost = remainingDays * ratePerHour;
+
+  const discountedMonthCost = monthCost * percentageAfter;
+  const discountedFullMonthsCost = months * discountedMonthCost;
+
+  const total = discountedFullMonthsCost + remainingCost;
+  return Math.ceil(total);
+}
+
+console.log(priceWithMonthlyDiscount(89, 230, 0.42));
